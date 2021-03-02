c7c3871ab7e45bf084280a7b39a82e55624dcedf
Bambam Deo Kumar <69498578+bkumarcredex@users.noreply.github.com>
2021-03-02 17:27:17 +0530

    Vindecoder issue fixed

--- Client/Model/VinBatch.cs
+++ Client/Model/VinBatch.cs
@@ -9,7 +9,7 @@ namespace Client.Model
     {
         public string AirBagLocFront { get; set; }
         public string AirBagLocSide { get; set; }
-            public string BodyClass { get; set; }
+        public string BodyClass { get; set; }
         public string FuelTypePrimary { get; set; }
         public string PlantCity { get; set; }
         public string PlantCountry { get; set; }
--- Client/Model/VinDecode.cs
+++ Client/Model/VinDecode.cs
@@ -10,11 +10,11 @@ namespace Client.Model
         public string BodyClass { get; set; }
         public string Make { get; set; }
         public string VIN { get; set; }
-        public int MakeId { get; set; }
+        public int? MakeId { get; set; }
         public string Manufacturer { get; set; }
-        public int ManufacturerId { get; set; }
+        public int? ManufacturerId { get; set; }
         public string Model { get; set; }
-        public int ModelId { get; set; }
+        public int? ModelId { get; set; }
         public string Trim { get; set; }
         public string FuelType { get; set; }
 
